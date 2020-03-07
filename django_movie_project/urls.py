2dca5c21ffc638aaeff0f62242ae3e18bc9f0a4e
KarstenPoddig <karsten_poddig@web.de>
2020-03-07 12:39:34 +0100

    Fix url- and deploment-file

--- django_movie_project/urls.py
+++ django_movie_project/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django_movie_project import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
